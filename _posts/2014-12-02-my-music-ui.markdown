0cad66f86bddf8d696a7fc0eb2617f7b85a0b450
zhongchiyu <zhongchiyu@gmail.com>
2015-01-14 11:59:27 +0800

    Compress images

--- _posts/2014-12-02-my-music-ui.markdown
+++ _posts/2014-12-02-my-music-ui.markdown
@@ -5,7 +5,7 @@ date:   2014-10-15 10:34:30
 categories: work
 pre: | 
        听到喜欢的音乐就随手做了一张界面。
-src: /images/mymusic.png
+src: /images/waterfall/mymusic.png
 ---
 
 听到喜欢的音乐就随手做了一张界面。
---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var imageResize = require('gulp-image-resize');
+var clean = require('gulp-clean');
+
+gulp.task('default', function() {
+    gulp.watch('_assets/*', function(event) {
+        gulp.src(event.path)
+            .pipe(imageResize({ 
+                width : 640,
+                crop : false,
+                upscale : false
+            }))
+            .pipe(imagemin({
+                progressive: true,
+                use: [pngquant()],
+            }))
+            .pipe(gulp.dest('images'));
+        gulp.src(event.path)
+            .pipe(imageResize({ 
+                width : 277,
+                crop : false,
+                upscale : false
+            }))
+            .pipe(imagemin({
+                progressive: true,
+                use: [pngquant()],
+            }))
+            .pipe(gulp.dest('images/waterfall'));
+    });
+});
+
+gulp.task('build', ['clean', 'minimize']);
+
+gulp.task('minimize', ['waterfall', 'post'], function() {
+    return gulp.src('dist/**/*')
+        .pipe(imagemin({
+            progressive: true,
+            use: [pngquant()],
+        }))
+        .pipe(gulp.dest('images'));
+});
+
+gulp.task('waterfall', function() {
+    return gulp.src('_assets/*')
+        .pipe(imageResize({ 
+            width : 277,
+            crop : false,
+            upscale : false
+        }))
+        .pipe(gulp.dest('dist/waterfall'));
+});
+
+gulp.task('post', function() {
+    return gulp.src('_assets/*')
+        .pipe(imageResize({ 
+            width : 640,
+            crop : false,
+            upscale : false
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function() {
+    return gulp.src(['images', 'dist'], {read: false})
+        .pipe(clean());
+});
